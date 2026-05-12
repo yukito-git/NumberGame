b75ab302442fbf4ff553fd60eb39963dac98fac7
o-yukito01 <k3a5b7u0s1i5k9i@gmail.com>
2026-05-12 12:33:13 +0900

    GUIファイルの消去・NumberGameの改善

--- NumberGame.java
+++ NumberGame.java
@@ -1,39 +1,8 @@
-import javax.swing.JFrame;
-import javax.swing.*;
-
 import java.util.Scanner;
 import java.util.Random;
+
 public class NumberGame{
     public static void main(String[] args){
-        JFrame frame = new JFrame("数字当てゲーム");
-
-        JButton button = new JButton("判定");
-        JTextField textField = new JTextField(10);
-        JLabel label = new Jlabel("1~100で当ててください");
-
-        int answer = (int)(Math.random() * 100) + 1;
-
-        frame.setLayout(new java.awt.FlowLayout());
-
-        frame.add(textField);
-        frame.add(button);
-        frame.add(label);
-
-        button.addActionListener(e -> {
-            String input = textField.getText();
-            int guess = Integer.parseInt(input);
-
-            if (guess > answer){
-                
-            }
-            System.out.println("入力された数字:" + guess);
-        });
-
-        frame.add(button);
-
-        frame.setSize(300, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
@@ -49,8 +18,8 @@ public class NumberGame{
         System.out.println("2：普通(1～100)");
         System.out.println("3：難しい(1～1000)");
 
-        
         int max = 0;
+
         System.out.print("選択肢:");
         int select = sc.nextInt();
 
@@ -59,13 +28,13 @@ public class NumberGame{
             System.out.print("選択肢:");
             select = sc.nextInt();
         }
+
         if(select == 1){
             max = 10;
             maxAttempts = 5;
         }else if(select == 2){
             max = 100;
             maxAttempts = 7;
-
         }else{
             max = 1000;
             maxAttempts = 10;
@@ -76,7 +45,8 @@ public class NumberGame{
 
         System.out.println("1～" + max + "の数字を当ててください");
 
-        while(guess != answer && count < maxAttempts){
+        while(count < maxAttempts){
+
             System.out.print("数字を入力:");
             guess = sc.nextInt();
             
@@ -85,34 +55,39 @@ public class NumberGame{
                 continue;
             }
 
-            if(guess == answer){
-            }else{
-                System.out.println("ゲームオーバー！");
-                System.out.println("正解は" + answer + "でした");
-            }
 
             count++;
 
+
             int diff = Math.abs(answer - guess);
 
-            if(guess < answer){
+            if(diff <= 3 && guess != answer){
+                System.out.println("かなり近い!");
+            }else if(guess < answer){
                 System.out.println("もっと大きいです");
             }else if(guess > answer){
                 System.out.println("もっと小さいです");
             }else{
                 System.out.println("正解です。");
                 System.out.println(count + "回で当てました。");
+                break;
             }
 
-            if(diff <= 3 && guess != answer){
-                System.out.println("かなり近い!");
-            }
-
+            System.out.println("残り" + (maxAttempts - count) + "回です");
+            
         }
+
+        if(guess != answer){
+            System.out.println("ゲームオーバー！");
+            System.out.println("正解は" + answer + "でした");
+        }
+
         System.out.println("もう一回やりますか？");
         System.out.println("1:はい");
         System.out.println("2:いいえ");
         System.out.print("選択肢:");
+
+        
         int again = sc.nextInt();
 
         if(again == 2){
